87e5cb1f8dc82eaa8ecebc847d23e768a2cb48f6
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-03-28 23:35:56 -0400

    feat: implement user authentication with signup and login forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,70 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "./config/firebase";
 import "./App.css";
 import Todos from "./pages/Todos";
 import React, { useState } from "react";
 
 function App() {
-  const [showLogin, setShowLogin] = useState(true);
+  const [showLogin, setShowLogin] = useState(false);
+  const [user, setUser] = useState(null);
+  const [showTodos, setShowTodos] = useState(false);
+
+  // Login form state
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  // Signup form state
+  const [signupEmail, setSignupEmail] = useState("");
+  const [signupPassword, setSignupPassword] = useState("");
+  const [signupConfirm, setSignupConfirm] = useState("");
+
+  // handle form submission
+  const handleSignupSubmit = async (e) => {
+    e.preventDefault();
+
+    if (signupPassword.trim().length >= 6 && signupPassword === signupConfirm) {
+      await createUserWithEmailAndPassword(auth, signupEmail, signupPassword);
+      alert("Signup successful! Please log in.");
+      setShowLogin(true);
+    } else {
+      alert(
+        "Passwords must be at least 6 characters and match the confirmation.",
+      );
+    }
+  };
+
+  // hande login form submission
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+    await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+    alert("Login successful!");
+    setShowLogin(false);
+    setShowTodos(true);
+  };
 
   const showLoginForm = () => (
     <div className="auth-form-container">
       <h2>Login</h2>
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={handleLoginSubmit}>
         <label htmlFor="email">Email:</label>
-        <input type="email" placeholder="Email" id="email" />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          value={loginEmail}
+          onChange={(e) => setLoginEmail(e.target.value)}
+        />
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" placeholder="Password" />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          value={loginPassword}
+          onChange={(e) => setLoginPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
       </form>
       <div className="auth-toggle" onClick={() => setShowLogin(false)}>
@@ -24,16 +76,30 @@ function App() {
   const showSignupForm = () => (
     <div className="auth-form-container">
       <h2>Sign Up</h2>
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={handleSignupSubmit}>
         <label htmlFor="signup-email">Email:</label>
-        <input type="email" placeholder="Email" id="signup-email" />
+        <input
+          type="email"
+          placeholder="Email"
+          id="signup-email"
+          value={signupEmail}
+          onChange={(e) => setSignupEmail(e.target.value)}
+        />
         <label htmlFor="signup-password">Password:</label>
-        <input type="password" id="signup-password" placeholder="Password" />
+        <input
+          type="password"
+          id="signup-password"
+          placeholder="Password"
+          value={signupPassword}
+          onChange={(e) => setSignupPassword(e.target.value)}
+        />
         <label htmlFor="signup-confirm">Confirm Password:</label>
         <input
           type="password"
           id="signup-confirm"
           placeholder="Confirm Password"
+          value={signupConfirm}
+          onChange={(e) => setSignupConfirm(e.target.value)}
         />
         <button type="submit">Sign Up</button>
       </form>
@@ -46,8 +112,7 @@ function App() {
   return (
     <>
       <h1>Firebase + React</h1>
-      {showLogin ? showLoginForm() : showSignupForm()}
-      <Todos />
+      {showLogin ? showLoginForm() : showTodos ? <Todos /> : showSignupForm()}
     </>
   );
 }
